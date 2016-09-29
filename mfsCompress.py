2a1b47f72783bf0bdc4c90ad1b14b725c903f877
Levi Muniz <levi.muniz17@gmail.com>
2016-09-29 09:26:16 -0600

    ADDED - Small class with an outline for compression while sending information

---
+++ mfsCompress.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import bz2
+
+class mfsCompress(object):
+	"Compresses data on the fly"
+
+	def compress(self, data):
+		return bz2.compress(data)
+				
+	def decompress(self, data):
+		return bz2.decompress(data)
+
