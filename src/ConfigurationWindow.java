321764252ec6aa4bcb9ac7990a172e26733a0757
Mark Hedrick <markrhedrick@gmail.com>
2016-10-17 20:40:59 -0600

    ADDED - Configuration Window Manipulation

--- src/ConfigurationWindow.java
+++ src/ConfigurationWindow.java
@@ -1,5 +1,26 @@
+import javax.swing.*;
+
 /**
  * Created by markhedrick on 10/15/16.
  */
-public class ConfigurationWindow {
+public class ConfigurationWindow extends JFrame {
+
+    private JPanel contentPane;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+
+    public ConfigurationWindow() {
+        setContentPane(contentPane);
+        setTitle("MeshFS - Initial Configuration");
+        setSize(300,200);
+        setResizable(false);
+        toFront();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void run() {
+        JFrame window = new ConfigurationWindow();
+        CenterWindow.centerOnScreen(window);
+        window.setVisible(true);
+    }
 }
--- src/WelcomeWindow.java
+++ src/WelcomeWindow.java
@@ -3,20 +3,27 @@
  */
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class WelcomeWindow extends JFrame {
 
     private JPanel contentPane;
-    private JButton configure;
+    private JButton configureBtn;
 
     public WelcomeWindow() {
         setContentPane(contentPane);
-        setTitle("This is a test title");
+        setTitle("MeshFS - Welcome");
         setSize(300,200);
         setResizable(false);
         toFront();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        configureBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onConfigure();
+            }
+        });
     }
 
     public static void run() {
@@ -24,4 +31,13 @@ public class WelcomeWindow extends JFrame {
         CenterWindow.centerOnScreen(window);
         window.setVisible(true);
     }
+
+    private void onConfigure() {
+        JFrame ConfigurationWindow = new ConfigurationWindow();
+        CenterWindow.centerOnScreen(ConfigurationWindow);
+        ConfigurationWindow.setVisible(true);
+        dispose();
+    }
+
+
 }
