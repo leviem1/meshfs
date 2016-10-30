0293d5e8a702c4a26d989b22694cb8458ae4a546
Levi Muniz <levi.muniz17@gmail.com>
2016-10-30 03:54:29 -0600

    ADDED - commons-cli support

---
+++ src/CliParser.java
@@ -0,0 +1,42 @@
+import org.apache.commons.cli.*;
+
+import java.util.Properties;
+
+/**
+ * Created by Levi Muniz on 10/30/16.
+ */
+public class CliParser {
+
+    private Options opt = new Options();
+
+    public CliParser(String[] args, Properties properties) {
+        opt.addOption("h", "help", false, "Display application's help message.");
+        opt.addOption("m", "masterIP", true, "IP of master (if self, use 127.0.0.1 or own IP).");
+        opt.addOption("r", "regenConfig", false, "Regenerate application's default configuration file.");
+        DefaultParser parser = new DefaultParser();
+        try {
+            CommandLine cmd = parser.parse(opt, args);
+
+            if (cmd.hasOption("h")) {
+                help();
+            }
+
+            if (cmd.hasOption("m")) {
+                properties.setProperty("masterIP", cmd.getOptionValue("m"));
+            }
+
+            if (cmd.hasOption("r")) {
+                MeshFS.writeDefaultProperties();
+            }
+
+        } catch (ParseException e) {
+            help();
+        }
+    }
+
+    private void help() {
+        HelpFormatter formater = new HelpFormatter();
+        formater.printHelp("MeshFS [options]", opt);
+        System.exit(0);
+    }
+}
--- src/MeshFS.java
+++ src/MeshFS.java
@@ -10,9 +10,7 @@ import java.util.*;
 
 public class MeshFS {
 
-    public static void main(String[] args) {
-
-        Properties properties;
+    public static Properties writeDefaultProperties() {
         Properties defaultProperties = new Properties();
         defaultProperties.setProperty("numStripes","3");
         defaultProperties.setProperty("numStripeCopy", "2");
@@ -20,28 +18,27 @@ public class MeshFS {
         defaultProperties.setProperty("minSpace", "0");
         defaultProperties.setProperty("masterIP","127.0.0.1");
         defaultProperties.setProperty("repository", ("repo" + File.separator));
+        ConfigParser.write(defaultProperties);
+
+        return defaultProperties;
+    }
+
+    public static void main(String[] args) {
+
+        Properties properties;
 
         try {
             properties = ConfigParser.reader();
         } catch (IOException io) {
-            properties = defaultProperties;
-            ConfigParser.write(properties);
+            properties = writeDefaultProperties();
         }
+
+        new CliParser(args, properties);
 
         File repo = new File(properties.getProperty("repository"));
 
         if (!repo.exists()) {
             repo.mkdirs();
-        }
-
-        if (args.length > 0) {
-            for (int i = 0; i <= args.length; i++) {
-                if (args[i].equals("--master") || args[i].equals("-m")) {
-                    properties.setProperty("masterIP", args[i+1]);
-                } else if (args[i].equals("--regenConfig") || args[i].equals("-r")){
-                    ConfigParser.write(defaultProperties);
-                }
-            }
         }
 
         //Welcome.run();
