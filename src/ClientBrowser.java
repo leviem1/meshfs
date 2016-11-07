c1555d59f881da0e71386bb9559a39b7fa7bd6ba
Mark Hedrick <markrhedrick@gmail.com>
2016-11-06 21:05:18 -0700

    ADDED - Client mesh browser window. (WIP)

---
+++ src/ClientBrowser.java
@@ -0,0 +1,113 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.*;
+import javax.swing.GroupLayout;
+import javax.swing.border.*;
+/*
+ * Created by JFormDesigner on Sun Nov 06 18:04:04 MST 2016
+ */
+
+
+
+/**
+ * @author Mark Hedrick
+ */
+public class ClientBrowser extends JFrame {
+    public ClientBrowser() {
+        initComponents();
+
+    }
+
+    public ArrayList convertToArray(JSONObject object){
+        JSONArray contents = (JSONArray) object.get("root");
+        ArrayList arrayList = contents;
+        return arrayList;
+    }
+
+    private void initComponents() {
+        ArrayList meshList = convertToArray(JSONReader.getJSONObject("/Users/markhedrick/Desktop/test.json"));
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Mark Hedrick
+        dialogPane = new JPanel();
+        contentPanel = new JPanel();
+        scrollPane1 = new JScrollPane();
+        tree1 = new JTree();
+        buttonBar = new JPanel();
+        okButton = new JButton();
+
+        //======== this ========
+        setTitle("MeshFS - Client Browser");
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        //======== dialogPane ========
+        {
+            dialogPane.setBorder(new EmptyBorder(12, 12, 12, 12));
+
+            // JFormDesigner evaluation mark
+            dialogPane.setBorder(new javax.swing.border.CompoundBorder(
+                new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
+                    "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+                    javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
+                    java.awt.Color.red), dialogPane.getBorder())); dialogPane.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+
+            dialogPane.setLayout(new BorderLayout());
+
+            //======== contentPanel ========
+            {
+
+                //======== scrollPane1 ========
+                {
+                    scrollPane1.setViewportView(tree1);
+                }
+
+                GroupLayout contentPanelLayout = new GroupLayout(contentPanel);
+                contentPanel.setLayout(contentPanelLayout);
+                contentPanelLayout.setHorizontalGroup(
+                    contentPanelLayout.createParallelGroup()
+                        .addGroup(contentPanelLayout.createSequentialGroup()
+                            .addComponent(scrollPane1, GroupLayout.DEFAULT_SIZE, 368, Short.MAX_VALUE)
+                            .addContainerGap())
+                );
+                contentPanelLayout.setVerticalGroup(
+                    contentPanelLayout.createParallelGroup()
+                        .addGroup(contentPanelLayout.createSequentialGroup()
+                            .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE)
+                            .addGap(0, 1, Short.MAX_VALUE))
+                );
+            }
+            dialogPane.add(contentPanel, BorderLayout.CENTER);
+
+            //======== buttonBar ========
+            {
+                buttonBar.setBorder(new EmptyBorder(12, 0, 0, 0));
+                buttonBar.setLayout(new GridBagLayout());
+                ((GridBagLayout)buttonBar.getLayout()).columnWidths = new int[] {0, 80};
+                ((GridBagLayout)buttonBar.getLayout()).columnWeights = new double[] {1.0, 0.0};
+
+                //---- okButton ----
+                okButton.setText("OK");
+                buttonBar.add(okButton, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 0, 0), 0, 0));
+            }
+            dialogPane.add(buttonBar, BorderLayout.SOUTH);
+        }
+        contentPane.add(dialogPane, BorderLayout.CENTER);
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Mark Hedrick
+    private JPanel dialogPane;
+    private JPanel contentPanel;
+    private JScrollPane scrollPane1;
+    private JTree tree1;
+    private JPanel buttonBar;
+    private JButton okButton;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
