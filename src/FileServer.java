751842db265f42d797380d8d8873110ac3f58c65
Levi Muniz <levi.muniz17@gmail.com>
2016-11-09 09:39:20 -0700

    ADDED - Receive requests

--- src/FileServer.java
+++ src/FileServer.java
@@ -1,7 +1,7 @@
 /**
  * Created by Levi Muniz on 10/16/16.
  */
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 
@@ -38,12 +38,28 @@ class ServerInit implements Runnable {
         this.timeout = timeout;
     }
 
+    private String receiveRequest(Socket client) {
+        String requestPart;
+        String requestFull = "";
+        try (
+                BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()))
+        ) {
+            while (((requestPart = input.readLine()) != null) && (requestFull.length() < 2048)) {
+                requestFull = requestFull + requestPart;
+            }
+
+            return requestFull;
+        } catch (IOException io) {
+            return requestFull;
+        }
+    }
+
     public void run() {
             while (!Thread.interrupted()) {
                 try {
                     Socket client = server.accept();
                     client.setSoTimeout(timeout);
-                    System.out.println("Fire!");
+                    String requestString = receiveRequest(client);
                     client.close();
                 } catch (IOException io) {
                     io.printStackTrace();
