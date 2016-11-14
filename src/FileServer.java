d2c8275ddf7e62fa6b66ac6ffba2fc67682a5a56
Levi Muniz <levi.muniz17@gmail.com>
2016-11-14 02:08:37 -0700

    ADDED - Ping and error responses over server connection
    (In short, I figured out how to make a server that works and is 99% idiot proof…. This was the hard part.)

--- src/FileServer.java
+++ src/FileServer.java
@@ -38,13 +38,65 @@ class ServerInit implements Runnable {
         this.timeout = timeout;
     }
 
+    private void processRequest(String request, PrintWriter out) {
+        if (request != null) {
+            try {
+                String[] requestParts = request.split("\\|");
+                if (requestParts[0].equals("101")) {            //101:Get file
+                    //sendFile(requestParts[1], out);
+
+                } else if (requestParts[0].equals("102")) {     //102:Post file
+                    //receiveFile(requestParts[1], out);
+
+                } else if (requestParts[0].equals("103")) {     //103:Move file (virtual only)
+                    //moveFile(requestParts[1], requestParts[2]);
+
+                } else if (requestParts[0].equals("104")) {     //104:Copy file (virtual only)
+                    //copyFile(requestParts[1], requestParts[2]);
+
+                } else if (requestParts[0].equals("105")) {     //105:Delete file (virtual and physical)
+                    //deleteFile(requestParts[1], requestParts[2]);
+
+                } else if (requestParts[0].equals("106")) {     //106:Make directory (virtual)
+                    //makeDir(requestParts[1]);
+
+                } else if (requestParts[0].equals("107")) {     //107:Get report
+                    //sendReport(out);
+
+                } else if (requestParts[0].equals("108")) {     //108:Post report
+                    //receiveReport(requestParts[0]);
+
+                } else if (requestParts[0].equals("109")) {     //109:Ping
+                    ping(out);
+
+                } else if (requestParts[0].equals("110")) {     //110:Bind
+                    //bindClient(requestParts[1], requestParts[2]);
+
+                } else {
+                    badRequest(out, request);
+                }
+            } catch (IndexOutOfBoundsException ioobe) {
+                badRequest(out, request);
+            }
+        }
+    }
+
+    private void ping(PrintWriter out) {
+        out.println("Pong!");
+    }
+
+    private void badRequest(PrintWriter out, String request) {
+            out.println("ERROR! Bad request:\n\n" + request);
+    }
+
     private String receiveRequest(Socket client) {
         String requestPart;
         String requestFull = "";
-        try (
-                BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()))
-        ) {
+        try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
             while (((requestPart = input.readLine()) != null) && (requestFull.length() < 2048)) {
+                if (requestPart.equals("EOR")) break;
                 requestFull = requestFull + requestPart;
             }
 
@@ -59,7 +111,8 @@ class ServerInit implements Runnable {
                 try {
                     Socket client = server.accept();
                     client.setSoTimeout(timeout);
-                    String requestString = receiveRequest(client);
+                    PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                    processRequest(receiveRequest(client), out);
                     client.close();
                 } catch (IOException io) {
                     io.printStackTrace();
