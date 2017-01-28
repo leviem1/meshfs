da2a151fd1d9483728fac4db6588b535e16438c8
Aaron Duran <amduran1999@gmail.com>
2017-01-28 00:25:39 -0700

    ADDED - dynamic reasigning of number of stripes and wholes based on number of computers active ADDED - lots of comments

--- src/DISTAL.java
+++ src/DISTAL.java
@@ -12,7 +12,9 @@ class DISTAL {
     private static LinkedHashMap<String, Long> valueSorter(LinkedHashMap<String,Long> storageMap){
 
         LinkedHashMap<String, Long> sortedMap = new LinkedHashMap();
-        sortedMap.put("temp", 99999999999990L);
+
+        //put something in the map to compare against
+        sortedMap.put("temp", 99999999999999L);
 
         for (String key : storageMap.keySet()){
             Long storageAmount = storageMap.get(key);
@@ -21,6 +23,7 @@ class DISTAL {
 
             for (String sortedKey : sortedMap.keySet()){
                 if (storageAmount >= sortedMap.get(sortedKey)){
+                    //reorder the map when a storage value is larger than one that is already in the map
                     LinkedHashMap<String,Long>  reorderStorageMap = (LinkedHashMap<String,Long>) sortedMap.clone();
                     sortedMap.clear();
                     for (String reorderKey : reorderStorageMap.keySet()){
@@ -39,6 +42,7 @@ class DISTAL {
                 sortedMap.put(key,storageMap.get(key));
             }
         }
+        //remove the temporary key, value combination
         sortedMap.remove("temp");
         return sortedMap;
     }
@@ -52,6 +56,7 @@ class DISTAL {
             userAccount = filePathInCatalog;
         }
 
+        //import properties
         int numOfStripes = Integer.valueOf(MeshFS.properties.getProperty("numStripes"));
         int numOfStripedCopies = Integer.valueOf(MeshFS.properties.getProperty("numStripeCopy"));
         int numOfWholeCopies = Integer.valueOf(MeshFS.properties.getProperty("numWholeCopy"));
@@ -60,34 +65,45 @@ class DISTAL {
         String manifestFileLocation = MeshFS.properties.getProperty("repository")+".manifest.json";
         JSONObject manifestFile = JSONManipulator.getJSONObject(manifestFileLocation);
         String catalogFileLocation = MeshFS.properties.getProperty("repository")+".catalog.json";
+
+        //make the JTree show that the file is being distributed
         JSONManipulator.addToIndex(userAccount, uploadFilePath.substring(uploadFilePath.lastIndexOf(File.separator)+1) + " (distributing)", catalogFileLocation, userAccount, true);
 
         try {
-             String fileName = uploadFilePath.substring(uploadFilePath.lastIndexOf(File.separator) + 1);
-             long sizeOfFile = FileUtils.getSize(uploadFilePath);
-             //long sizeOfFile = 5000000L;
-             long sizeOfStripe = ((sizeOfFile / numOfStripes) + 1);
+            String fileName = uploadFilePath.substring(uploadFilePath.lastIndexOf(File.separator) + 1);
+            long sizeOfFile = FileUtils.getSize(uploadFilePath);
+            long sizeOfStripe = ((sizeOfFile / numOfStripes) + 1);
 
-             LinkedHashMap<String, Long> compStorageMap = JSONManipulator.createStorageMap(manifestFile);
-             LinkedHashMap<String, Long> sortedCompStorageMap = valueSorter(compStorageMap); //sort the compStorageMap by descending available storage
+            //create a map of the amount of available storage on each computer
+            LinkedHashMap<String, Long> compStorageMap = JSONManipulator.createStorageMap(manifestFile);
 
-            for (int storage = 0; storage < sortedCompStorageMap.size(); storage++){ // account for the desired amount of free space
-                 String macAddress = String.valueOf(sortedCompStorageMap.keySet().toArray()[storage]);
-                 sortedCompStorageMap.replace(macAddress, sortedCompStorageMap.get(String.valueOf(macAddress)) - minFreeSpace);
+            //sort the compStorageMap by descending available storage
+            LinkedHashMap<String, Long> sortedCompStorageMap = valueSorter(compStorageMap);
+
+            for (int storage = 0; storage < sortedCompStorageMap.size(); storage++){
+                // account for the desired amount of free space
+                String macAddress = String.valueOf(sortedCompStorageMap.keySet().toArray()[storage]);
+                sortedCompStorageMap.replace(macAddress, sortedCompStorageMap.get(String.valueOf(macAddress)) - minFreeSpace);
             }
-             /*//uncomment me for dynamic resigning of numStripes by number of computers that are on
-             int numOfComputersUsed = sortedCompStorageMap.size();
-             if (numOfComputersUsed < (numOfWholeCopies + (numOfStripedCopies * numOfStripes))) {
-                 numOfStripes = ((numOfComputersUsed - numOfWholeCopies) / numOfStripedCopies);
-             }
-             //*/
-             //create a unique filename for the uploaded file
-             List<String> computersForWholes = new ArrayList<>();
-             JSONObject jsonObj = JSONManipulator.getJSONObject(catalogFileLocation);
-             String currentName = jsonObj.get("currentName").toString();
-             String newName = incrementName(currentName);
 
-             //determine which computers can hold the full file
+            //dynamic resigning of number of Wholes by number of computers that are on
+            int numOfComputersUsed = sortedCompStorageMap.size();
+            if (numOfComputersUsed > numOfWholeCopies){
+                numOfWholeCopies = numOfComputersUsed;
+            }
+
+            //dynamic resigning of number of Stripes by number of computers that are on
+            if (numOfComputersUsed < (numOfWholeCopies + (numOfStripedCopies * numOfStripes))) {
+                numOfStripes = ((numOfComputersUsed - numOfWholeCopies) / numOfStripedCopies);
+            }
+
+            //create a unique filename for the uploaded file
+            List<String> computersForWholes = new ArrayList<>();
+            JSONObject jsonObj = JSONManipulator.getJSONObject(catalogFileLocation);
+            String currentName = jsonObj.get("currentName").toString();
+            String newName = incrementName(currentName);
+
+            //determine which computers can hold the full file
             int stopOfWholes = (-1);
             for (int computerNumW = 0; computerNumW < numOfWholeCopies; computerNumW++) {
                 String macAddress = String.valueOf(sortedCompStorageMap.keySet().toArray()[computerNumW]);
@@ -100,6 +116,7 @@ class DISTAL {
                 stopOfWholes = computerNumW;
             }
 
+            //determine which computers can hold a striped file
             int lastResortComp = 0;
             int lapNum = 1;
             List<String> computersForStripes = new ArrayList<>();
@@ -115,6 +132,8 @@ class DISTAL {
                 if ((!macAddress.equals("none") && (sortedCompStorageMap.get(macAddress) - (sizeOfStripe * lapNum)) >= sizeOfStripe)) {
                     computersForStripes.add(macAddress);
                 } else if (computerNumS != 0){
+
+                    //find a computer that can hold the stripe, even if one computer gets two stripes
                     String macAddressNew;
                     long availableStorage;
 
@@ -131,10 +150,11 @@ class DISTAL {
                         macAddressNew = "none";
                     }
 
-
                     if (availableStorage >= sizeOfStripe) {
                         computersForStripes.add(macAddressNew);
                     }
+
+                    //iterate over all the computers again to find a place for the stripe, if a computer has not already been found
                     else {
                         lapNum++;
                         lastResortComp = 0;
@@ -154,6 +174,7 @@ class DISTAL {
                             }
                         }
                         if (lastResortComp >= sortedCompStorageMap.size()){
+                            //no more computers have the storage for a stripe
                             break;
                         }
                     }
@@ -164,9 +185,14 @@ class DISTAL {
                 }
             }
 
+
+            //create the list that will be used to distribute the wholes and stripes
             List<List<String>> stripes = new ArrayList<>();
+
+            //frist list is for the computers that will receive wholes
             stripes.add(computersForWholes);
 
+            //if only computer is available to hold stripes, then do not use stripes.
             boolean allowStripes = false;
             for (String computer : computersForStripes){
                 if (!computer.equals(computersForStripes.get(0))){
@@ -175,51 +201,59 @@ class DISTAL {
                 }
             }
 
-             if (allowStripes){
-                 for (int copy = 0; copy < numOfStripes; copy++) {
-                     stripes.add(new ArrayList<>());
-                 }
-                 for (int copy = 0; copy < numOfStripedCopies; copy++) {
-                     for (int currentStripe = 0; currentStripe < numOfStripes; currentStripe++){
-                         try{
-                             int test = 0;
 
-                             while (true) {
-                                 test++;
-                                 if (test >= (computersForStripes.size() - (copy * numOfStripes) + currentStripe)){
-                                     break;
-                                 }
+            if (allowStripes){
+                //create a list for each stripe
+                for (int copy = 0; copy < numOfStripes; copy++) {
+                    stripes.add(new ArrayList<>());
+                }
 
-                                 String computerToReceive = computersForStripes.get((copy * numOfStripes) + currentStripe);
+                //balancing the number of computers that each stripe is sent to.
+                for (int copy = 0; copy < numOfStripedCopies; copy++) {
+                    for (int currentStripe = 0; currentStripe < numOfStripes; currentStripe++){
+                        try{
+                            int test = 0;
 
-                                 if (stripes.get(currentStripe + 1).size() > 0){
-                                     boolean isNotBroken = true;
-                                     for (String tempMac : stripes.get(currentStripe + 1)) {
-                                         if (tempMac.equals(computerToReceive)) {
-                                             computersForStripes.add(tempMac);
-                                             computersForStripes.remove((copy * numOfStripes) + currentStripe);
-                                             isNotBroken = false;
-                                             break;
-                                         }
-                                     }
-                                     if (isNotBroken){
-                                         stripes.get(currentStripe + 1).add(computerToReceive);
-                                         break;
-                                     }
-                                 } else {
-                                     stripes.get(currentStripe + 1).add(computerToReceive);
-                                     break;
-                                 }
-                             }
-                         } catch (Exception e) {
-                             System.out.println("You are out of Storage!");
-                         }
-                     }
-                 }
-             }
+                            while (true) {
+                                test++;
+                                if (test >= (computersForStripes.size() - (copy * numOfStripes) + currentStripe)){
+                                    break;
+                                }
+
+                                String computerToReceive = computersForStripes.get((copy * numOfStripes) + currentStripe);
+
+                                //ensure that no one computer gets two or more of the same stripe
+                                if (stripes.get(currentStripe + 1).size() > 0){
+                                    boolean isNotBroken = true;
+                                    for (String tempMac : stripes.get(currentStripe + 1)) {
+                                        if (tempMac.equals(computerToReceive)) {
+                                            computersForStripes.add(tempMac);
+                                            computersForStripes.remove((copy * numOfStripes) + currentStripe);
+                                            isNotBroken = false;
+                                            break;
+                                        }
+                                    }
+                                    if (isNotBroken){
+                                        stripes.get(currentStripe + 1).add(computerToReceive);
+                                        break;
+                                    }
+                                } else {
+                                    stripes.get(currentStripe + 1).add(computerToReceive);
+                                    break;
+                                }
+                            }
+                        } catch (Exception e) {
+                            System.out.println("You are out of Storage!");
+                        }
+                    }
+                }
+            }
+
+            //send the files to the respective computers
             sendFiles(stripes,uploadFilePath,sizeOfFile,newName);
-            String itemLocation = userAccount + "/" + uploadFilePath.substring(uploadFilePath.lastIndexOf(File.separator)+1) + " (distributing)";
-            JSONManipulator.writeJSONObject(catalogFileLocation, JSONManipulator.removeItem(jsonObj, itemLocation));
+
+            //update the JSON file in order to update the JTree
+            JSONManipulator.writeJSONObject(catalogFileLocation, JSONManipulator.removeItem(jsonObj, userAccount + "/" + uploadFilePath.substring(uploadFilePath.lastIndexOf(File.separator)+1) + " (distributing)"));
             JSONManipulator.addToIndex(stripes, filePathInCatalog, fileName, catalogFileLocation, newName, userAccount, sizeOfFile);
 
         } catch (Exception e) {
@@ -228,17 +262,19 @@ class DISTAL {
     }
 
     private static String incrementName(String name){
+        //creates a new unique filename
         String alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         int incrementReverseIndex = 1;
         String toAdd = "";
 
-
         while(true) {
             try {
+                //advance to the next character.
                 char lastChar = name.charAt(name.length()-incrementReverseIndex);
                 toAdd += alphabet.charAt(alphabet.indexOf(lastChar) + 1);
                 break;
             } catch (IndexOutOfBoundsException iobe) {
+                //if have reached the end of the alphabet, loop back to the beginning and use the next character
                 toAdd += alphabet.charAt(0);
                 incrementReverseIndex++;
             }
@@ -253,16 +289,21 @@ class DISTAL {
     private static void sendFiles (List<List<String>> stripes, String sourceFileLocationOld, long fileSize, String outName){
         List<Thread> parentThreads = new ArrayList<>();
         long sizeOfStripe = ((fileSize / (stripes.size() -1) + 1));
+
+        //rename the original file to what the distributed whole file will be
         new File(sourceFileLocationOld).renameTo(new File(sourceFileLocationOld.substring(0,sourceFileLocationOld.lastIndexOf(File.separator)+1) + outName + "_w"));
         final String sourceFileLocation = sourceFileLocationOld.substring(0, sourceFileLocationOld.lastIndexOf(File.separator) + 1) + outName + "_w";
         JSONObject manifestFile = JSONManipulator.getJSONObject(MeshFS.properties.getProperty("repository")+".manifest.json");
 
+        //create a new thread for each stripe, so all stripes are written simultaneously
         for (int stripe = -1; stripe < (stripes.size()-1); stripe++){
             parentThreads.add(new Thread(new sendFilesTreading(sizeOfStripe, fileSize, sourceFileLocation, manifestFile, stripes, stripe, outName)));
         }
         for (Thread parent : parentThreads) {
             parent.start();
         }
+
+        //wait for all the treads to close
         for (Thread parent : parentThreads) {
             if (parent.isAlive()) {
                 try {
@@ -272,6 +313,7 @@ class DISTAL {
             }
         }
 
+        //delete the original file
         FileUtils.removeFile(sourceFileLocation);
     }
 }
@@ -299,6 +341,7 @@ class sendFilesTreading implements Runnable{
         List<Thread> childThreads = new ArrayList<>();
 
         if (stripe == -1){
+            //send the wholes
             for (String computerToReceive : stripes.get(stripe+1)) {
                 Thread child = new Thread(() -> {
                     try {
@@ -312,7 +355,7 @@ class sendFilesTreading implements Runnable{
             }
         }
         else if (stripe == stripes.size()-2) {
-
+            //send the last stripe, taking into account that he the last stripe to usually smaller than other stripes.
             FileUtils.writeStripe(sourceFileLocation, MeshFS.properties.getProperty("repository") + File.separator + outName + "_s" + stripe, (sizeOfStripe * stripe), sizeOfStripe - ((sizeOfStripe * (stripes.size() - 1)) - fileSize));
             for (String computerToReceive : stripes.get(stripe+1)){
                 Thread child = new Thread(() -> {
@@ -326,6 +369,7 @@ class sendFilesTreading implements Runnable{
             }
         }
         else {
+            //send all other stripes
             FileUtils.writeStripe(sourceFileLocation, MeshFS.properties.getProperty("repository") + File.separator + outName + "_s" + stripe, (sizeOfStripe * stripe), sizeOfStripe);
             for (String computerToReceive : stripes.get(stripe + 1)) {
                 Thread child = new Thread(() -> {
@@ -339,10 +383,13 @@ class sendFilesTreading implements Runnable{
                 childThreads.add(child);
             }
         }
+
+        //send all copies of stripe simultaneously
         for (Thread child : childThreads) {
             child.start();
         }
 
+        //wait for all copies to finish sending before deleting the stripe on the master
         for (Thread child : childThreads) {
             if (child.isAlive()) {
                 try {
