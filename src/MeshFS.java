179d1b4b9d159de70c7d40cc6e5f0b5ba4049400
Mark Hedrick <markrhedrick@gmail.com>
2017-01-30 12:02:58 -0700

    REMOVED: unused code.

--- src/MeshFS.java
+++ src/MeshFS.java
@@ -127,15 +127,6 @@ public class MeshFS {
 
         }
     }
-    public static void setUIFont (javax.swing.plaf.FontUIResource f){
-        java.util.Enumeration keys = UIManager.getDefaults().keys();
-        while (keys.hasMoreElements()) {
-            Object key = keys.nextElement();
-            Object value = UIManager.get (key);
-            if (value != null && value instanceof javax.swing.plaf.FontUIResource)
-                UIManager.put (key, f);
-        }
-    }
 }
 
 class onQuit implements Runnable {
