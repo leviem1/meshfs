c5c40c537514714b145f4fb303a3da4a7f677b56
Levi Muniz <levi.muniz17@gmail.com>
2017-04-25 22:09:44 -0600

    ADDED - Download functionality ADDED - File listing functionality CHANGED - Slimmed import statements

--- src/DriveAPI.java
+++ src/DriveAPI.java
@@ -1,20 +1,15 @@
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.*;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.googleapis.media.MediaHttpDownloader;
-import com.google.api.client.googleapis.media.MediaHttpUploader;
-import com.google.api.client.http.FileContent;
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpTransport;
+import com.google.api.client.googleapis.media.*;
+import com.google.api.client.http.*;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.DriveScopes;
-import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.*;
+import com.google.api.services.drive.model.*;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -58,7 +53,8 @@ public class DriveAPI {
         return insert.execute();
     }
 
-    static void downloadFile(File uploadedFile) throws Exception {
+    static void downloadFile(String fileID) throws Exception {
+
         java.io.File parentDir = new java.io.File(System.getProperty("user.home") + java.io.File.separator + "Downloads" + java.io.File.separator);
         if (!parentDir.exists() && !parentDir.mkdirs()) {
           throw new IOException("Unable to create parent directory");
@@ -69,12 +65,22 @@ public class DriveAPI {
         Credential credential = authorize(JSONFactory, httpTransport);
         Drive drive = new Drive.Builder(httpTransport, JSONFactory, credential).setApplicationName("MeshFS-MeshFS/1.0").build();
 
+        File uploadedFile = drive.files().get(fileID).execute();
+
         OutputStream out = new FileOutputStream(new java.io.File(parentDir, uploadedFile.getTitle()));
 
         MediaHttpDownloader downloader =
             new MediaHttpDownloader(httpTransport, drive.getRequestFactory().getInitializer());
         downloader.setDirectDownloadEnabled(false);
         downloader.download(new GenericUrl(uploadedFile.getDownloadUrl()), out);
-  }
+    }
+
+    static FileList listFiles() throws Exception {
+        JsonFactory JSONFactory = JacksonFactory.getDefaultInstance();
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        Credential credential = authorize(JSONFactory, httpTransport);
+        Drive drive = new Drive.Builder(httpTransport, JSONFactory, credential).setApplicationName("MeshFS-MeshFS/1.0").build();
+        return drive.files().list().setQ("trashed = false").execute();
+    }
 }
 
