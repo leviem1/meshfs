e4aea97dce904a558edd76bce4f4be77745ee3e8
Levi Muniz <levi.muniz17@gmail.com>
2017-06-07 12:46:31 -0600

    CHANGED - MalformedRequestException Javadoc

--- src/MalformedRequestException.java
+++ src/MalformedRequestException.java
@@ -2,8 +2,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * The MalformedRequestException class is an exception thrown when
- * a request does not exist or is invalid
+ * This exception is thrown when a request does not exist or is invalid
  *
  * @author Levi Muniz
  * @version 1.0.0
@@ -13,10 +12,23 @@ class MalformedRequestException extends Exception {
 
     private List<String> messageParts;
 
+    /**
+     * Creates the MalformedRequestException.
+     *
+     * @param message   the server's response
+     */
+
     MalformedRequestException(String message) {
         super("Request did not exist or was invalid");
         messageParts = Arrays.asList(message.split(";"));
     }
+
+    /**
+     * This method returns the reason for the MalformedRequestException
+     *
+     * @return a list of strings with the request (index 0)
+     *         and the cause (index 1)
+     */
 
     List<String> getReason() {
         return messageParts;
