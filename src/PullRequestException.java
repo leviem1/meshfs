c0ccf4450c0c0e711a8e36063832f1c65fe47d13
Aaron Duran <amduran1999@gmail.com>
2017-06-07 12:33:23 -0600

    ADDED - PullRequestException JavaDoc

--- src/PullRequestException.java
+++ src/PullRequestException.java
@@ -1,5 +1,9 @@
 /**
+ * The PullRequestException class is an exception thrown when
+ * a full file cannot be restored with the files on the system
+ *
  * @author Aaron Duran
+ * @version 1.0.0
  */
 
 class PullRequestException extends Exception {
