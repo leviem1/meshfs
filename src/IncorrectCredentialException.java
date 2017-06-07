9a35d0e6123596b90ac3c15562a9e1db94217dda
Levi Muniz <levi.muniz17@gmail.com>
2017-06-07 12:41:20 -0600

    ADDED - IncorrectCredentialException Javadoc

--- src/IncorrectCredentialException.java
+++ src/IncorrectCredentialException.java
@@ -1,8 +1,15 @@
 /**
+ * This exception occurs on credential mismatch
+ *
  * @author Levi Muniz
  */
 
 class IncorrectCredentialException extends Exception {
+
+    /**
+     * Creates the IncorrectCredentialException.
+     */
+
     IncorrectCredentialException() {
         super("Incorrect username or password");
     }
